8b1ccfa7b5c0378aaa639a2d162e96b9a8c66731
myserver <ihorkondrashov@gmail.com>
2025-08-15 14:37:57 +0000

    Transactions

--- task.sql
+++ task.sql
@@ -1,18 +1,7 @@
-USE ShopDB; 
-
--- Some data should be created outside the transaction (here)
-INSERT INTO Orders (CustomerID, Date)
-VALUES (1, '2025-01-01');
-
--- Start the transaction 
+INSERT INTO Orders (CustomerID, Date) VALUES (1, '2025-01-01');
+SET @order_id = LAST_INSERT_ID();
+SET @current_amount = (SELECT WarehouseAmount FROM Products WHERE Name = 'AwersomeProduct');
 START TRANSACTION; 
-
--- And some data should be created inside the transaction 
-INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (LAST_INSERT_ID(), 1, 1);
-
-UPDATE Products 
-SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = 1;
-
+UPDATE Products SET WarehouseAmount = @current_amount - 1 WHERE ID = 1;
+INSERT INTO OrderItems (OrderId, ProductId, Count) VALUES (@order_id,1,1);
 COMMIT; 
